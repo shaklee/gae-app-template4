fe02e7f6285417c22e718d841a0d74e7c4f2eccd
ealbek <ealbek-contractor@shaklee.com>
2017-05-08 16:34:14 -0700

    Added to springfox javax.validation annotations support

--- src/main/java/hello/ApiDocConfiguration.java
+++ src/main/java/hello/ApiDocConfiguration.java
@@ -2,9 +2,11 @@ package hello;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import com.google.common.base.Predicate;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,17 +18,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class ApiDocConfig {
+@Import(BeanValidatorPluginsConfiguration.class)
+public class ApiDocConfiguration {
 
 	@Bean
 	public Docket customDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				// PUBLIC API
-				.groupName("public").select().paths(paths()).build()
-		; 
+				.groupName("public").select().paths(publicPaths()).build()
+		// Internal API
+
+		;
 	}
 
-	private Predicate<String> paths() {
-		return Paths.paths("/greeting", "/hello/*");
+	private Predicate<String> publicPaths() {
+		return Paths.paths("/greeting");
 	}
 }
